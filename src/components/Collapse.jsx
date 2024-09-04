5fb620422953891fc6ae108fb0a99122c0c1fa0b
Natacha-R <166326801+Natacha-R@users.noreply.github.com>
2024-09-04 19:09:52 +0200

    style page A Propos

---
+++ src/components/Collapse.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import "../css/pages/layout.css";
+import "../css/pages/about.css";
+
+const Collapse = ({ title, children }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleCollapse = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <div className="collapse-container">
+      <div className="collapse-header">
+        <h2>{title}</h2>
+        <i
+          className={`fas fa-chevron-down chevron-icon ${
+            isOpen ? "rotated" : ""
+          }`}
+          onClick={toggleCollapse}
+        ></i>
+      </div>
+      {isOpen && <div className="collapse-content">{children}</div>}
+    </div>
+  );
+};
+
+export default Collapse;
