45221020d242054efa7271405143390222d498d7
Natacha-R <166326801+Natacha-R@users.noreply.github.com>
2024-09-05 10:41:25 +0200

    page error

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import image from "../assets/404.png";
 import { NavLink } from "react-router-dom";
 
@@ -8,7 +7,10 @@ function Error() {
     <main className="error">
       <img src={image} alt="logo erreur 404" />
       <p> Oups! La page que vous demandez n'existe pas. </p>
-      <NavLink to="/"> Retourner sur la page d'accueil </NavLink>
+      <NavLink to="/" className="error-nav">
+        {" "}
+        Retourner sur la page d'accueil{" "}
+      </NavLink>
     </main>
   );
 }
