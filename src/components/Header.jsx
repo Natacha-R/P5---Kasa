fe159c1d48a5a8fea875e13d212ac2d1c446d689
Natacha-R <166326801+Natacha-R@users.noreply.github.com>
2024-09-12 22:54:58 +0200

    debut media queries page home

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { NavLink } from "react-router-dom"; /*système d'ancre*/
 
 import Logo from "../assets/logoH.png";
+import SmallLogo from "../assets/logo_mobile.png";
 
 function Header() {
   return (
